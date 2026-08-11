335aecfae05052591707b1517640e89bc577d98c
codytran11 <codytran76@gmail.com>
2026-08-11 15:44:19 -0700

    Initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h1>Hi! I'm Cody</h1>
+      <p>Computer Science & Business Administration 2nd Year @ USC</p>
+    </section>
+  );
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+function Navbar() {
+  return <h1>Cody Tran</h1>;
+}
+
+export default Navbar;
